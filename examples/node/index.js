1dbc1394758834834874e247c1e4bedaea760e2c
cliffsindia <cmelyn15@gmail.com>
2024-12-13 09:50:05 +0000

    character error

--- examples/node/index.js
+++ examples/node/index.js
@@ -2,7 +2,7 @@ const pdf2excel = require('../../lib');
 const path = require('path');
 
 try {
-  pdf2excel.genXlsx(path.resolve(__dirname, '../bar.pdf'), 'bar.xlsx', {
+  pdf2excel.genXlsx(path.resolve(__dirname, '../mypdf.pdf'), 'bar.xlsx', {
     onProgress: e => console.warn(`${e.numPage} / ${e.numPages}`),
   });
 } catch (err) {
